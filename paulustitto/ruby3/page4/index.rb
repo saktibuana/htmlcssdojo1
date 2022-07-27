5c6910d115ba2be6127c8ae76cbb5c45cfbe75c2
Paul Titto <paulustito555@gmail.com>
2022-07-27 23:00:08 +0700

    Adding File

---
+++ paulustitto/ruby3/page4/index.rb
@@ -0,0 +1,10 @@
+# Define the print_info method
+def print_info(item)
+  puts "Welcome to Ninja Electronics!"
+  puts "#{item}s are on sale today!"
+# Call the print_info method with the argument "Headphone"
+end
+
+# Call the print_info method with the argument "TV"
+print_info("Headphone")
+print_info("TV")
