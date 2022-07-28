24e4c94d6825e87fa57f9fd293a9d21dcba92c19
imana <missimana@gmail.com>
2022-07-28 08:49:30 +0700

    Imana's learning progress

---
+++ Imana_Malia_Kondou/ruby_study_2/page3/index.rb
@@ -0,0 +1,9 @@
+languages = ["Jepang", "Inggris", "Spanyol"]
+
+# Dapatkan masing masing element dari variable `languages` menggunakan method `each`,
+# kemudian cetak "Saya bisa berbahasa ____"
+
+languages.each do |language|
+  puts language
+  puts "Saya bisa berbahasa #{language}"
+end
