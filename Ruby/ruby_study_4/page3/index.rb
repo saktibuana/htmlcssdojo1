65f93631b86656381223ac234e397ecad6f32fb9
Tati Nurhayati <tatinurrr@gmail.com>
2022-07-28 18:46:26 +0700

    upload

--- Ruby/ruby_study_4/page3/index.rb
+++
@@ -1,5 +0,0 @@
-class Menu
-  # Tambahkan variable instance `name` dan `price`
-  attr_accessor :name
-  attr_accessor :price
-end
--- hanifsyamsudin/Ruby/ruby_study_2/page9/index.rb
+++
@@ -1,7 +0,0 @@
-exam = {subject: "Matematika", score: 80}
-
-# Cetak nilai dari simbol :grade
-puts exam ["grade"]
-
-# Cetak nil
-puts nil
