5d78b635459a0d16bfb1db7aecc6fcb7168127c6
decom21 <henripardamean@kemenparekraf.go.id>
2022-07-28 22:31:45 +0700

    update ruby5

---
+++ ruby_study_5/page11/index.rb
@@ -0,0 +1,12 @@
+# Lakukan `import` class `Date` menggunakan `require`
+require "date"
+
+# Tetapkan sebuah instance dari Date pada variable `birthday`
+birthday=Date.new(2022, 7, 17)
+
+
+# Cetak variable `birthday`
+puts birthday
+
+# Cetak nilai `return` dari method instance `sunday?` dari variable `birthday`
+puts birthday.sunday?
