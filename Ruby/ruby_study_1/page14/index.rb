377cafab4fb7be61cc738b6bdb4ec63f75c6a705
imana <missimana@gmail.com>
2022-07-28 08:56:28 +0700

    my learning progress

---
+++ Ruby/ruby_study_1/page14/index.rb
@@ -0,0 +1,15 @@
+score = 100
+
+# Jika nilai `score` adalah 100, cetak "Bagus sekali!"
+if score == 100
+  puts "Bagus sekali!"
+end
+
+# Jika nilai `score` tidak bernilai 100, cetak "Anda bisa lebih baik lagi!"
+if score != 100
+  puts "Anda bisa lebih baik lagi!"
+end
+if puts score != 100
+  puts "Anda bisa lebih baik lagi!"
+end
+
