51f582e93166eb968d3b906019ee6a95018b665e
hanifsyamsudin <109723202+hanifsyamsudin@users.noreply.github.com>
2022-07-28 11:31:59 +0700

    upload ruby study 5

---
+++ Ruby/ruby_study_5/page11/index.rb
@@ -0,0 +1,11 @@
+# Lakukan `import` class `Date` menggunakan `require`
+require "date"
+
+# Tetapkan sebuah instance dari Date pada variable `birthday`
+birthday = Date.new(1995,7,18)
+
+# Cetak variable `birthday`
+puts birthday
+
+# Cetak nilai `return` dari method instance `sunday?` dari variable `birthday`
+puts birthday.sunday?
