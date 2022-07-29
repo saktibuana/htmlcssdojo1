3c9074dce74c1e3946cca4c8b27c63b19c7fc768
fathuranan <fathur.anan@gmail.com>
2022-07-29 19:29:30 +0700

    tugas pertama html css 1 fathurrahman

--- ruby_study_3/page10/index.rb
+++
@@ -1,8 +0,0 @@
-# Rewrite the parameters to receive keyword arguments
-def buy(item:, price:, count:)
-	puts "You have bought #{count} #{item}s"
-	puts "The total price is $#{price * count}"
-end
-
-# Rewrite the arguments with keyword arguments
-buy(item: "headphone", price: 150, count: 2)
--- ruby_study_3/page2/index.rb
+++
@@ -1,8 +0,0 @@
-# Define print_info method
-def print_info
-  puts "Welcome to Ninja Electronics!"
-  puts "Headphones are on sale today!"
-end
-
-# Call print_info method
-print_info
