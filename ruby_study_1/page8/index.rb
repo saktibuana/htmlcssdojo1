e2ef8c40ca64cbf4d82b4b95e66fdfb9146b995f
syahidbausad <syahidbausad@gmail.com>
2022-07-30 21:25:21 +0900

    first upload

--- ruby_study_1/page8/index.rb
+++
@@ -1,7 +0,0 @@
-# Assign " is an awesome programming language" to the text variable
-text = " is an awesome programming language"
-
-# Concatenate the following strings with the text variable and print the result
-puts "Ruby" + text
-puts "Python" + text
-puts "Java" + text
--- rubystudy2/page12/index.rb
+++
@@ -1,8 +0,0 @@
-exams = [
-  {subject: "Math", score: 80},
-  {subject: "Science", score: 55}
-]
-
-# Print value of the element with the symbol :score at index 1
-
-puts exams[1][:score]
