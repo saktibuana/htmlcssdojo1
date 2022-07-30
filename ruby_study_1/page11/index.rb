7b35b9d5862733e54ca1532ca878134c45305cf6
okiesupiawan <okie.amd.kom@gmail.com>
2022-07-30 20:51:11 +0700

    Pertama Upload Di Branch

--- ruby_study_1/page11/index.rb
+++
@@ -1,6 +0,0 @@
-length = 9
-width = 8
-area = length * width
-
-# Print "The area is ____" using the area variable
-puts "The area is #{area}"
--- ruby_study_1/page12/index.rb
+++
@@ -1,6 +0,0 @@
-score = 92
-
-# If the score is greater than 80, print "Great job!"
-if score > 80
-  puts "Great job!"
-end
