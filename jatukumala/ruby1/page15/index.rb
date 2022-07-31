bc9b102d6d67e5078d78e185e0b4068be8600a00
sarijk <jatuksari@gmail.com>
2022-07-31 07:47:37 +0700

    revisi

--- jatukumala/ruby1/page15/index.rb
+++
@@ -1,8 +0,0 @@
-score = 60
-
-# Tambahkan sebuah pernyataan `else` untuk mencetak "Anda bisa lebih baik lagi!"
-if score > 80
-  puts "Bagus sekali!"
-else
-  puts "Anda bisa lebih baik lagi!"
-  end
