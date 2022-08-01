2be6d6088ac9eea56dce3d6a372a1316d7fedd5e
abdurrahmansholeh <pbitvsda@gmail.com>
2022-08-01 19:29:45 +0700

    Pertama Uload ddi Git

--- ruby_study_1/page2/index.rb
+++
@@ -1,5 +0,0 @@
-# Print "Hello Ruby"
-puts "Hello Ruby"
-
-# Make the following line a comment
-# puts "Hello Progate"
--- ruby_study_1/page6/index.rb
+++
@@ -1,5 +0,0 @@
-# Assign the string "Ken the Ninja" to the name variable
-name = "Ken the Ninja"
-
-# Print the name variable
-puts name
