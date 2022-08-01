525e393b76fc3738e75ca8ea091f7892ee3ad288
akakaze12 <kaze_harri@yahoo.com>
2022-08-01 21:54:57 +0700

    Add Ruby rails2, 3

---
+++ harriwahyudi/rails5_study_2/tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220801133622');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-08-01 13:42:28.798468','2022-08-01 13:42:28.798468');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Belajar Rails dengan Progate!','2022-08-01 13:45:06.752455','2022-08-01 13:45:06.752455');
+INSERT INTO posts VALUES(2,'Mencoba menampilkan post!','2022-08-01 13:45:58.980453','2022-08-01 13:45:58.980453');
+INSERT INTO posts VALUES(3,'Belajar code itu sangat menyenangkan!','2022-08-01 13:52:30.273018','2022-08-01 13:52:30.273018');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
---
+++ harriwahyudi/rails5_study_3/tweet_app/db/.backup.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220131045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-01-31 05:21:00.324397','2022-01-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Mencari buku yang bagus untuk dibaca.','2022-01-31 05:24:15.529063','2022-01-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'Makan siang hari ini enak sekali!','2022-01-31 05:24:32.004378','2022-01-31 05:24:32.004378');
+INSERT INTO posts VALUES(3,'Belajar Rails bersama Progate!','2022-08-01 14:16:12.033878','2022-08-01 14:16:12.033878');
+INSERT INTO posts VALUES(4,'ini test perdana','2022-08-01 14:51:47.368826','2022-08-01 14:51:47.368826');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',4);
+COMMIT;
