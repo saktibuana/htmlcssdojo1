75a1959b391c1691f0c1c9b85ab401a3632fc481
Ari Yudianto <arbenyudianto@gmail.com>
2022-08-02 01:48:29 +0700

    Update Task Rails5 Study 2&3

---
+++ rails5_study_2/tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220729145808');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-07-29 14:58:51.093252','2022-07-29 14:58:51.093252');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Learning Rails with Progate!','2022-07-29 15:03:32.454824','2022-07-29 15:03:32.454824');
+INSERT INTO posts VALUES(2,'Trying to display the posts!','2022-07-29 15:04:13.773133','2022-07-29 15:04:13.773133');
+INSERT INTO posts VALUES(3,'Learning to code is so much fun!','2022-07-29 15:08:24.077426','2022-07-29 15:08:24.077426');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
