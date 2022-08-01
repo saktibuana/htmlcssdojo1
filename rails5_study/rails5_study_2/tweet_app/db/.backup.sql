a3310fb918c3f10c1942ba225b5d7736b57c8796
dayatfadila <dayat.recovery@gmail.com>
2022-08-01 08:31:16 +0700

    rails study 2

---
+++ rails5_study/rails5_study_2/tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220801011229');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-08-01 01:13:25.659292','2022-08-01 01:13:25.659292');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Belajar Rails bersama Progate!','2022-08-01 01:18:23.146753','2022-08-01 01:18:23.146753');
+INSERT INTO posts VALUES(2,'Mencoba menampilkan post!','2022-08-01 01:18:34.311102','2022-08-01 01:18:34.311102');
+INSERT INTO posts VALUES(3,'Belajar coding itu sangat menyenangkan!','2022-08-01 01:23:46.882478','2022-08-01 01:23:46.882478');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
