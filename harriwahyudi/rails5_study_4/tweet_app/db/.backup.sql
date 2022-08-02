df7e916ed05bd4d6d6c2aa7aca27867f85177131
akakaze12 <kaze_harri@yahoo.com>
2022-08-02 21:52:43 +0700

    add Ruby Rail IV

---
+++ harriwahyudi/rails5_study_4/tweet_app/db/.backup.sql
@@ -0,0 +1,11 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-03-31 05:21:00.324397','2022-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(3,'Belajar Rails bersama Progate!','2022-04-06 07:12:40.797561','2022-04-06 07:12:40.797561');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
