20eccfe83adcaff2132c6cad35c867ea102a98ac
imana <missimana@gmail.com>
2022-08-02 19:27:53 +0700

    Dojo-Ruby on Rails5 II

---
+++ Ruby on Rails5/rails5_dojo_2/tweet_app/db/.backup.sql
@@ -0,0 +1,11 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220512024421');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-05-12 02:44:26.268623','2022-05-12 02:44:26.268623');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(3,'Saat Anda menekan tombol Hapus, post yang sedang ditampilkan harus terhapus.','2022-08-02 12:12:53.589291','2022-08-02 12:12:53.589291');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
