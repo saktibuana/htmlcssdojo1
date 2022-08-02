de65aa5e4888a138c9f3e984229e5e5227913995
febiyani paneo <febiyani_s1arsitektur@mahasiswa.ung.ac.id>
2022-08-02 22:25:14 +0800

    proggress until ruby study 5

---
+++ ruby_study_3/page1/index.rb
@@ -0,0 +1,11 @@
+def introduce
+  puts "Halo"
+  puts "Saya adalah Ninja Ken"
+  puts "Saya berusia 14 tahun"
+  # Print "Saya berusia 14 tahun"
+  
+end
+
+puts "-----Perkenalan diri-----"
+# Panggil method introduce
+introduce
