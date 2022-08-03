f13be8e5579284dd40f7df1efed2a12b3c36999c
Rizky Hermawan <rhermawan56@gmail.com>
2022-08-03 22:42:56 +0700

    update rails study_5

---
+++ Rizky Hermawan/rails_5/study_5/tweet_app/db/.backup.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Looking for a good book to read.','2017-03-31 05:24:15.529063','2017-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'Today''s lunch was great!','2017-03-31 05:24:32.004378','2017-03-31 05:24:32.004378');
+INSERT INTO posts VALUES(4,'I think it''s time for a short break... Master Wooly was kind enough to buy me some coffee and cake.','2022-08-03 14:10:47.999346','2022-08-03 14:10:47.999346');
+INSERT INTO posts VALUES(5,'Need to review what I learned today!','2022-08-03 14:12:32.159577','2022-08-03 14:12:32.159577');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',17);
+COMMIT;
