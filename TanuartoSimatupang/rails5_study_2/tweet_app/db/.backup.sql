474b063e9f8fd38d8f745c68e3347403ce139bb9
Atho Simatupang <tsmtpng@gmail.com>
2022-08-04 17:32:26 +0700

    Ruby on Rails5 Study 2

---
+++ TanuartoSimatupang/rails5_study_2/tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220804014013');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-08-04 01:42:27.311521','2022-08-04 01:42:27.311521');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Belajar Rails dengan Progate!','2022-08-04 01:57:19.197284','2022-08-04 01:57:19.197284');
+INSERT INTO posts VALUES(2,'Mencoba menampilkan post!','2022-08-04 01:58:21.306950','2022-08-04 01:58:21.306950');
+INSERT INTO posts VALUES(3,'Belajar code itu sangat menyenangkan!','2022-08-04 06:10:30.007997','2022-08-04 06:10:30.007997');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
