9fdcb40bbb5948d2f5e14abe4a510036efd03c08
sarijk <jatuksari@gmail.com>
2022-08-07 13:33:05 +0700

    live session day 9

---
+++ JatuKumalaSari/rubyrails5/tweet_app/db/.backup.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-03-31 05:21:00.324397','2022-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Mencari sebuah buku yang bagus untuk dibaca.','2022-03-31 05:24:15.529063','2022-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'Makan siang hari ini enak!','2022-03-31 05:24:32.004378','2022-03-31 05:24:32.004378');
+INSERT INTO posts VALUES(3,'','2022-08-06 07:43:33.784525','2022-08-06 07:43:33.784525');
+INSERT INTO posts VALUES(4,'Perlu meninjau kembali apa yang telah saya pelajari hari ini!','2022-08-06 07:49:03.549218','2022-08-06 07:49:03.549218');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',4);
+COMMIT;
