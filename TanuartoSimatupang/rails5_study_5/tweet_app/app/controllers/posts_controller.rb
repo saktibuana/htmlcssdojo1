9244d85a82d5e21abccf27ff8448a168c5b2abdf
Atho Simatupang <tsmtpng@gmail.com>
2022-08-07 21:30:38 +0700

    Ruby on Rails5 Study 5

---
+++ TanuartoSimatupang/rails5_study_5/tweet_app/app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      # Simpanlah pesan dalam flash[:notice]
+      flash[:notice] = "Post berhasil dibuat"
+      
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "Post berhasil diubah"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    # Simpanlah pesan dalam flash[:notice]
+    flash[:notice] = "Post berhasil dihapus"
+    
+    redirect_to("/posts/index")
+  end
+  
+end
