d6cdfb094b7563b7dafb7bb6c1f09f878b4ac7a0
hanifsyamsudin <109723202+hanifsyamsudin@users.noreply.github.com>
2022-08-21 13:37:08 +0700

    Delete kebumen_aspirasi_v1 directory

--- kebumen_aspirasi_v1/test/controllers/investasi_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class InvestasiControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get investasi_index_url
-    assert_response :success
-  end
-end
