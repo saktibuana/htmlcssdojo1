d91bc5b869942fef6c50ba63347e13452ae1d551
Nazario Rirang Limba <rio.nazario@gmail.com>
2022-08-23 21:55:19 +0700

    upload rails dojo 2

---
+++ rails5_dojo_2/tweet_app/db/.backup.sql
@@ -0,0 +1,11 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220512024421');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-05-12 02:44:26.268623','2022-05-12 02:44:26.268623');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'abc','2022-08-09 09:06:15.209036','2022-08-09 09:06:15.209036');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',1);
+COMMIT;
