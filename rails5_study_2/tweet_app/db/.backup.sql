a3a3f78d68db6d63c54661ebf2112e1e962e09f4
Nazario Rirang Limba <rio.nazario@gmail.com>
2022-08-23 21:47:54 +0700

    upload rails study 2

---
+++ rails5_study_2/tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220808024029');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-08-08 02:46:49.056742','2022-08-08 02:46:49.056742');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Belajar Rails dengan Progate!','2022-08-08 02:48:22.683033','2022-08-08 02:48:22.683033');
+INSERT INTO posts VALUES(2,'Mencoba menampilkan post!','2022-08-08 02:48:48.343830','2022-08-08 02:48:48.343830');
+INSERT INTO posts VALUES(3,'Belajar code itu sangat menyenangkan!','2022-08-08 02:58:12.148544','2022-08-08 02:58:12.148544');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
