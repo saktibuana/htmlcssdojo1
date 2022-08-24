63811bc7bf9c870eb3d74f219e7478874d2b9bc0
dayatfadila <dayat.recovery@gmail.com>
2022-08-24 20:49:20 +0700

    add more study and dojo rails 5

---
+++ rails5_study/rails5_dojo_3/tweet_app/db/.backup.sql
@@ -0,0 +1,18 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+INSERT INTO schema_migrations VALUES('20170418070645');
+INSERT INTO schema_migrations VALUES('20220823085541');
+INSERT INTO schema_migrations VALUES('20220823090136');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'I wonder what I should read next...','2017-03-31 05:24:15.529063','2017-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'Lunch was delicious today!','2017-03-31 05:24:32.004378','2017-03-31 05:24:32.004378');
+CREATE TABLE IF NOT EXISTS "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "email" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, "image_name" varchar, "password" varchar);
+INSERT INTO users VALUES(1,'fa','fad@dxd.anime','2022-08-23 09:43:42.333530','2022-08-23 09:43:42.333530',NULL,'xxx');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',2);
+INSERT INTO sqlite_sequence VALUES('users',1);
+COMMIT;
